1375d4feb6ac5bdc5aab9d9031749485a4152055
Abhis-123 <me19b069@smail.iitm.ac.in>
2021-09-12 10:13:28 +0530

    added dataset

---
+++ dataset.py
@@ -0,0 +1,28 @@
+from torch.utils.data import Dataset
+from torchvision.io import read_image
+
+
+class CharacterDataset(Dataset):
+    def __init__(self,dataframe=None,transform=None):
+        """
+        parameters:
+            dataframe : a pandas dataframe for reading files it must have 'path' and 'label' attributes
+            transform : function to transform the image data 
+
+        """
+        self.dataframe = dataframe
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.dataframe)
+
+    def __getitem__(self, index):
+        row = self.dataframe.iloc[index]
+        image= read_image(row['path'])
+        label= row['label']
+
+        if self.transform:
+            image= self.transform(image)
+        return image,label   
+
+
---
+++ main.py
@@ -0,0 +1,8 @@
+
+import time
+t1= time.time()
+import torch
+
+x= torch.Tensor([1,2,3,4,5])
+print(x)
+print(t1 -time.time())
